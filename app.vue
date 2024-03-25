0eee3e5c72137dcec7b809660697d514879dc661
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-24 21:29:18 -0300

    tailwind and style added

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+const version = 2 + 1
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <div class="text-3xl font-bold underline">
+      Hello Nuxt {{ version }}!
+    </div>
   </div>
 </template>
