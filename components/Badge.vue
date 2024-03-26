f924157949fcd90f88bfd8a25516daef550d9317
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-26 01:25:59 -0300

    badges updated

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,11 +1,6 @@
 <template>
   <span
-    class="bg-blue-100 text-teal-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-teal-900 dark:text-teal-300">
+    class="text-xs font-medium px-2.5 py-1  bg-teal-700 text-teal-100 cursor-default">
     <slot />
   </span>
-  <!-- <div class="h-auto w-fit rounded-lg text-xs font-medium bg-gray-100 dark:bg-gray-700 dark:text-gray-300">
-    <span class="px-3">
-      <slot />
-    </span>
-  </div> -->
 </template>
