4b18df8497eb0ba28131af58c93bda736e29896c
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-26 00:18:40 -0300

    navbar color links updated

--- components/app/Navbar.vue
+++ components/app/Navbar.vue
@@ -20,11 +20,17 @@ import IconOasis from '~/assets/icons/logo.svg'</script>
         <nav class="flex overflow-x-auto font-semibold space-x-8 justify-between md:justify-start items-center">
 
             <IconOasis class="size-8 text-teal-200" :fontControlled="false" alt="oasis icon" />
+
             <a class="font-extrabold text-teal-400 hover:text-teal-200 visited:text-teal-600" href="#about">{{
                 name }}
             </a>
-            <Ref :href="route.ref" class="hidden md:block hover:text-teal-200 visited:text-teal-600"
-                v-bind:key="route.name" v-for="(route) in routes">{{ route.name }}</Ref>
+            <div class="hidden md:block text-teal-300 hover:text-teal-200 visited:text-teal-600" v-bind:key="route.name"
+                v-for="(route) in routes">
+                <Ref :href="route.ref">
+                    {{ route.name }}
+                </Ref>
+            </div>
+
             <!-- md dropdown -->
             <button v-on:click="showDrop = !showDrop" class="md:hidden">
                 <Icon class="text-red-100" size="1.5rem" name="material-symbols-light:menu-rounded" />
@@ -33,9 +39,9 @@ import IconOasis from '~/assets/icons/logo.svg'</script>
         <div class="flex flex-col pt-4 text-center">
             <div :class="showDrop ? 'display' : 'hidden'" class="md:hidden space-y-1">
                 <div v-bind:key="route.name" v-for="(route) in routes"
-                    class="hover:text-teal-200 visited:text-teal-600">
-                    <Ref v-on:click="showDrop = !showDrop" class="hover:text-teal-200 visited:text-teal-600"
-                    :href="route.ref">{{ route.name }}</Ref>
+                    class="text-teal-300 hover:text-teal-200 visited:text-teal-600">
+                    <Ref v-on:click="showDrop = !showDrop" :href="route.ref">{{ route.name
+                        }}</Ref>
                 </div>
             </div>
         </div>
--- components/nav/Ref.vue
+++ components/nav/Ref.vue
@@ -4,9 +4,8 @@ defineProps({
 })
 </script>
 <template>
-    <a
-        :href="href"
-        class=" relative after:bg-white after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
+    <a :href="href" :class="$attrs.class"
+        class="font-bold relative after:bg-white after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer">
         <slot />
     </a>
 
