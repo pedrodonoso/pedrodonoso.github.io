a372be72f1bda6f252cfcddacb03f1980648a5ec
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-27 19:50:40 -0300

    skill item color font in skill and contact item updated

--- components/contact/ContactItem.vue
+++ components/contact/ContactItem.vue
@@ -8,7 +8,7 @@ defineProps({
     <a class="flex flex-row items-center gap-2 p-3 text-sm rounded-xl bg-teal-900 hover:-translate-y-1 duration-200 md:rounded-md lg:min-h-16"
         :href="href" target=”_blank”>
         <Icon :name="icon" class="text-teal-100 size-7 md:size-9" />
-        <div v-if="$slots.default" class="text-l hidden lg:block px-2">
+        <div v-if="$slots.default" class="font-semibold text-md text-teal-50 hidden lg:block">
             <slot />
         </div>
     </a>
--- components/skills/SkillItem.vue
+++ components/skills/SkillItem.vue
@@ -4,5 +4,5 @@ defineProps({
 })
 </script>
 <template>
-    <div class="w-fit py-2 px-6 text-l bg-teal-900 text-center hover:-translate-y-1 duration-200 cursor-default">{{ title }}</div>
+    <div class="w-fit py-2 px-6 font-semibold text-sm md:text-md text-teal-50 bg-teal-900 text-center hover:-translate-y-1 duration-200 cursor-default">{{ title }}</div>
 </template>
