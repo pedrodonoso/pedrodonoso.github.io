d5f9f3ea5814681e25a4c95bda1fa6e24c80218a
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-27 00:51:15 -0300

    tailwind updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  tailwindcss: {
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config",
+    exposeConfig: false,
+    exposeLevel: 2,
+    config: {},
+    injectPosition: "first",
+    viewer: true,
+  },
   devtools: { enabled: false },
-  modules: ["nuxt-icon", "nuxt-svgo", "@nuxt/content", "@vueuse/nuxt"],
+  modules: [
+    "nuxt-icon",
+    "nuxt-svgo",
+    "@nuxt/content",
+    "@vueuse/nuxt",
+    "@nuxtjs/tailwindcss",
+  ],
   svgo: {
     autoImportPath: "./assets/icons/",
   },
@@ -25,6 +40,20 @@ export default defineNuxtConfig({
         h2: "MyProseH2",
         p: "MyProseP",
       },
+    },
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://unpkg.com/transition-style",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+        },
+      ],
     },
   },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  important: true,
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
