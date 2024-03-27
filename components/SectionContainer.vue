dd5cda22db20535dd18b0f3e017f69f8207724fe
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-27 16:39:37 -0300

    color of  container title updated

--- components/SectionContainer.vue
+++ components/SectionContainer.vue
@@ -7,7 +7,7 @@ const props = defineProps({
 </script>
 <template>
     <section :id="id" class="text-left space-y-12">
-        <h2 class="text-3xl font-extrabold text-teal-400">
+        <h2 class="text-3xl font-extrabold text-teal-100">
             <Icon size="2.2rem" :name=icon /> {{ title }}
         </h2>
         <slot />
