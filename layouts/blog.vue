a64595974d7ca32ae2d92ab9c9e6a87147b0a13f
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-31 00:00:51 -0300

    blog layout added

---
+++ layouts/blog.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar />
+  <main class="flex flex-row">
+    <!-- <aside class="w-1/4 hidden md:block" > -->
+    <aside class="hidden md:block" >
+      <slot name="aside" />
+    </aside>
+    <!-- <section class="flex w-full"> -->
+    <section class="flex flex-col w-screen justify-center text-left p-6 md:p-12 min-h-[calc(100vh-136px)]">
+      <slot />
+    </section>
+  </main>
+  <Footer />
+</template>
