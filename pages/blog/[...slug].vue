dcf721000fef984fa9dcd4ff7e0dbb609e204797
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-31 00:02:57 -0300

    blog page updated with table-contents, not-found, and blog-layout

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,10 +1,24 @@
+<script setup lang="ts">
+const { page, toc } = useContent();
+</script>
 <template>
-  <NuxtLayout>
-    <ContentDoc v-slot="{ doc }">
-      <article class="text-left p-6 md:p-12">
-        <Breadcrumb :path="doc._path" class="pb-3" />
-        <ContentRenderer :value="doc" />
-      </article>
-    </ContentDoc>
+  <NuxtLayout name="blog">
+    <template
+      #aside
+      v-if="toc && toc.links.length !== 0"
+      :class="page ? 'block' : 'hidden'"
+    >
+      <PageToc />
+    </template>
+    <template #default>
+      <Breadcrumb :path="page._path" class="pb-3" v-if="page" />
+      <ContentRenderer :value="page">
+        <template #not-found>
+          <NotFound />
+        </template>
+        <template #empty>
+          <NotFound />
+        </template> </ContentRenderer
+    ></template>
   </NuxtLayout>
 </template>
