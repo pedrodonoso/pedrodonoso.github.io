32a948cafb96941f01a3cd244e4678438894d5d1
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-30 23:56:31 -0300

    not found added

---
+++ components/error-pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <div class="p-4">
+      <h1 class="text-4xl font-bold text-center">404</h1>
+      <h2 class="text-2xl font-semibold text-center">Página no encontrada</h2>
+      <div class="text-center">
+        <p>La página que buscas no existe o ha sido eliminada.</p>
+      </div>
+    </div>
+    <CustomButton>Vuelva al Blog</CustomButton>
+  </div>
+</template>
