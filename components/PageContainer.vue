b8a95af30c2d8a3dda52db91877ab3852d8fb43b
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-30 23:54:06 -0300

    page container of blog main added

--- components/PageContainer.vue
+++ components/PageContainer.vue
@@ -1,38 +1,36 @@
 <script setup>
 defineProps({
-    title: String,
-    subtitle: String,
-    description: String,
-    tags: Array,
-    url: String,
-    is_copyright: Boolean,
-    is_trademark: Boolean,
-    is_registered: Boolean
-})
+  title: String,
+  subtitle: String,
+  description: String,
+  tags: Array,
+  url: String,
+});
 </script>
 
 <template>
-    <article class="flex justify-left items-center gap-6 space-y-7 text-pretty bg-teal-700 w-full p-9">
-        <div class="flex flex-col text-left space-y-8 gap-6 w-full">
-            <div class="space-y-3 md:space-y-4">
-                <h1 class="flex text-xl font-extrabold">
-                    {{ title }}
-                    <Icon v-if="is_copyright" class="size-3 align-top pt-0.1" name="icon-park-outline:copyright" />
-                    <Icon v-if="is_trademark" class="size-3.5 align-top" name="ph:trademark" />
-                    <Icon v-if="is_registered" class="size-3-5 align-top" name="ph:trademark-registered" />
-                </h1>
-                <p class="text-xs md:text-sm leading-tight text-teal-100">{{ subtitle }}</p>
-                <p class="text-sm md:text-base md:leading-tight"> {{ description }}</p>
-                <div class="flex justify-between">
-                    <nav class="flex gap-x-2">
-                        <!-- <badge-container>
-                            <Badge v-bind:key="tag" v-for="(tag) in tags">{{ tag }} </Badge>
-                        </badge-container> -->
-                    </nav>
-                    <social class="text-2xl lg:text-3xl items-end" :web="url" :redirect="false" />
-                </div>
-
-            </div>
+  <a class="" :href="url">
+    <article
+      class="flex justify-left items-center gap-6 space-y-7 text-pretty bg-teal-700 w-full p-9 hover:-translate-y-1 duration-200"
+    >
+      <div class="flex flex-col text-left space-y-8 gap-6 w-full">
+        <div class="space-y-3 md:space-y-4">
+          <h1 class="flex text-xl font-extrabold">
+            {{ title }}
+          </h1>
+          <p class="text-xs md:text-sm leading-tight text-teal-100">
+            {{ subtitle }}
+          </p>
+          <p class="text-sm md:text-base md:leading-tight">{{ description }}</p>
+          <div class="flex justify-between">
+            <nav class="flex gap-x-2">
+              <badge-container>
+                <Badge v-bind:key="tag" v-for="tag in tags">{{ tag }} </Badge>
+              </badge-container>
+            </nav>
+          </div>
         </div>
+      </div>
     </article>
+  </a>
 </template>
