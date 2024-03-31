7d48c63ea4bac90ce70267fb435ebae1fd584e3c
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-30 23:56:13 -0300

    custom button added

---
+++ components/app/CustomButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLink to="/blog" class="text-teal-500 hover:underline">
+    <button
+      class="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4"
+    >
+      <slot />
+    </button>
+  </NuxtLink>
+</template>
