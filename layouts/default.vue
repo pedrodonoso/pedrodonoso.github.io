e620f2964c0da4d58e6ef3ee69a55557c7d413a5
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-31 00:01:21 -0300

    default layout updated

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="font-mono select-none">
     <Navbar />
-    <main class="flex justify-center p-6 md:p-12">
-      <section class="w-screen max-w-3xl md:max-w-2xl md:p-0">
+    <main class="flex justify-center p-6 md:p-12 min-h-[calc(100vh-136px)]">
+      <section class="flex flex-col justify-center w-screen max-w-3xl md:max-w-2xl md:p-0">
         <slot />
       </section>
     </main>
