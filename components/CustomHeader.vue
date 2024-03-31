82b482b9121de3bafcfdd80c34599a9d9b449e8b
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-03-30 23:51:47 -0300

    header blog main added

---
+++ components/CustomHeader.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  subtitle: String,
+  id: String,
+});
+</script>
+<template>
+  <div class="grid gap-4">
+    <h1 :id="id" class="text-left text-3xl font-extrabold text-teal-50">
+      {{ title }}
+    </h1>
+    <h2 class="text-left text-md font-extrabold text-teal-100">
+      {{ subtitle }}
+    </h2>
+  </div>
+</template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,10 +1,15 @@
 <template>
   <NuxtLayout>
-    <h1>Bienvenid@ a mi blog</h1>
-    <p>Aquí puedes encontrar un sinfín de temas de mi interés.</p>
-    <div class="grid gap-4">
+    <div>
+      <custom-header
+        title="Bienvenid@!"
+        subtitle="Aquí puedes encontrar un sinfín de temas de mi interés."
+      ></custom-header>
+    </div>
+
+    <div class="grid gap-4 py-12">
       <ContentList path="/blog" v-slot="{ list }">
-        <div class="bg-teal-300" v-for="article in list" :key="article._path">
+        <div class="" v-for="article in list" :key="article._path">
           <page-container
             :key="article._path"
             v-bind="article"
